ec30b1d70253a0bdbd7457fa88285cc2c65e8be3
kashishp <163237337+iamkp18@users.noreply.github.com>
2026-05-02 06:16:14 +0530

    final

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'http://taskmanager-production-9f59.up.railway.app' });
 
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem('token');
